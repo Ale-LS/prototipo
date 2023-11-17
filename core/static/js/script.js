a7e239b1c2ca08549c7d787e26bace2a36ec1d1f
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2023-11-17 06:39:42 -0300

    Creacion de paginas y scripts

---
+++ core/static/js/script.js
@@ -0,0 +1,23 @@
+//NAVBAR//
+$(document).ready(function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("menu-superior").outerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "menu.html", true);
+  xhttp.send();
+});
+
+//FOOTER//
+$(document).ready(function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("footer").outerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "footer.html", true);
+  xhttp.send();
+});
