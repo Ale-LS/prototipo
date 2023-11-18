dcd5e2995908506b081eaa5a3a5534260344a07d
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2023-11-18 14:40:58 -0300

    Se crearon las paginas de Registro y Inicio de Sesion

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,4 +1,4 @@
-//NAVBAR//
+//MENU TIPO NAVBAR//
 $(document).ready(function () {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -20,4 +20,184 @@ $(document).ready(function () {
   };
   xhttp.open("GET", "footer.html", true);
   xhttp.send();
+});
+
+//METODO DE RUT CHILENO//
+$.validator.addMethod(
+  "rutChileno",
+  function (value, element) {
+    // Eliminar puntos y guión del RUT
+    value = value.replace(/[.-]/g, "");
+    // Validar que el RUT tenga 8 o 9 dígitos
+    if (value.length < 8 || value.length > 9) {
+      return false;
+    }
+
+    // Validar que el último dígito sea un número o una 'K'
+    var validChars = "0123456789K";
+    var lastChar = value.charAt(value.length - 1).toUpperCase();
+    if (validChars.indexOf(lastChar) == -1) {
+      return false;
+    }
+
+    // Calcular el dígito verificador
+    var rut = parseInt(value.slice(0, -1), 10);
+    var factor = 2;
+    var sum = 0;
+    var digit;
+    while (rut > 0) {
+      digit = rut % 10;
+      sum += digit * factor;
+      rut = Math.floor(rut / 10);
+      factor = factor === 7 ? 2 : factor + 1;
+    }
+    var dv = 11 - (sum % 11);
+    dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+
+    // Validar que el dígito verificador sea correcto
+    return dv === lastChar;
+  },
+  "Por favor ingrese un RUT válido."
+);
+
+//REGISTRO//
+$(document).ready(function () {
+  $("#sign-up-form").validate({
+    rules: {
+      rut: {
+        required: true,
+        rutChileno: true,
+      },
+      nombre: {
+        required: true,
+      },
+      apellido: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      direccion: {
+        required: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+      password2: {
+        required: true,
+        equalTo: "#password",
+      },
+    },
+    messages: {
+      rut: {
+        required: "Por favor ingrese su RUT",
+        rutChileno: "Por favor ingrese un RUT válido",
+      },
+      nombre: {
+        required: "Por favor ingrese su nombre",
+      },
+      apellido: {
+        required: "Por favor ingrese su apellido",
+      },
+      email: {
+        required: "Por favor ingrese su correo electrónico",
+        email: "Por favor ingrese un correo electrónico válido",
+      },
+      password: {
+        required: "Por favor ingrese su contraseña",
+        minlength: "La contraseña debe tener al menos 5 caracteres",
+      },
+      password2: {
+        required: "Por favor repita su contraseña",
+        equalTo: "Las contraseñas no coinciden",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  //Botón Guardar
+  $("#id_btnGuardar").on("click", function () {
+    if ($("#sign-up-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#sign-up-form").submit();
+    }
+  });
+
+  //Boton Limpiar
+  $("#id_btnLimpiar").click(function() {
+    $("#sign-up-form").reset();
+  });
+});
+
+//LOGIN//
+$(document).ready(function () {
+  $("#login-form").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+        minlength: 5,
+      },
+    },
+    messages: {
+      email: {
+        required: "Por favor ingrese su correo electrónico",
+        email: "Por favor ingrese un correo electrónico válido",
+      },
+      password: {
+        required: "Por favor ingrese su contraseña",
+        minlength: "La contraseña debe tener al menos 5 caracteres",
+      },
+    },
+    errorElement: "div",
+    errorPlacement: function (error, element) {
+      error.addClass("invalid-feedback");
+      element.closest(".form-group").append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element)
+        .addClass(errorClass)
+        .removeClass(validClass)
+        .closest(".form-group")
+        .addClass("has-error");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element)
+        .removeClass(errorClass)
+        .addClass(validClass)
+        .closest(".form-group")
+        .removeClass("has-error");
+    },
+  });
+
+  //Botón Iniciar Sesion
+  $("#id_btnIniciarSesion").on("click", function () {
+    if ($("#login-form").valid()) {
+      // El formulario es válido, se puede enviar
+      $("#login-form").submit();
+    }
+  });
 });
