48357d48c9e82d6069aaec19ea0a864d153e8a66
Ale <69171801+Ale-LS@users.noreply.github.com>
2023-11-19 21:37:59 -0300

    varios cambios

--- core/views.py
+++ core/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 def inicio(request):
         return render(request, 'core/inicio.html')
+
+def ingresar(request):
+        return render(request, 'core/ingresar.html')
+
+def nosotros(request):
+        return render(request, 'core/nosotros.html')
+
+def registro(request):
+        return render(request, 'core/registro.html')
 # Create your views here.
