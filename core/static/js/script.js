86af9beef3f95acd6faa6eb5f884e9f45fb0d529
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2023-11-20 00:11:06 -0300

    Se agregaron vistas para el usuario Paciente

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
       document.getElementById("menu-superior").outerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "menu.html", true);
+  xhttp.open("GET", "navbar.html", true);
   xhttp.send();
 });
 
@@ -146,58 +146,5 @@ $(document).ready(function () {
   //Boton Limpiar
   $("#id_btnLimpiar").click(function() {
     $("#sign-up-form").reset();
-  });
-});
-
-//LOGIN//
-$(document).ready(function () {
-  $("#login-form").validate({
-    rules: {
-      email: {
-        required: true,
-        email: true,
-      },
-      password: {
-        required: true,
-        minlength: 5,
-      },
-    },
-    messages: {
-      email: {
-        required: "Por favor ingrese su correo electrónico",
-        email: "Por favor ingrese un correo electrónico válido",
-      },
-      password: {
-        required: "Por favor ingrese su contraseña",
-        minlength: "La contraseña debe tener al menos 5 caracteres",
-      },
-    },
-    errorElement: "div",
-    errorPlacement: function (error, element) {
-      error.addClass("invalid-feedback");
-      element.closest(".form-group").append(error);
-    },
-    highlight: function (element, errorClass, validClass) {
-      $(element)
-        .addClass(errorClass)
-        .removeClass(validClass)
-        .closest(".form-group")
-        .addClass("has-error");
-    },
-    unhighlight: function (element, errorClass, validClass) {
-      $(element)
-        .removeClass(errorClass)
-        .addClass(validClass)
-        .closest(".form-group")
-        .removeClass("has-error");
-    },
-  });
-
-  //Botón Iniciar Sesion
-  $("#id_btnIniciarSesion").on("click", function () {
-    if ($("#login-form").valid()) {
-      // El formulario es válido, se puede enviar
-      $("#login-form").submit();
-    }
   });
 });
---
+++ core/static/js/script_paciente.js
@@ -0,0 +1,23 @@
+//MENU TIPO NAVBAR//
+$(document).ready(function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("menu-superior").outerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "navbar_paciente.html", true);
+  xhttp.send();
+});
+
+//FOOTER//
+$(document).ready(function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("footer").outerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "footer.html", true);
+  xhttp.send();
+});
